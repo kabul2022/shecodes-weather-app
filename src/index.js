21212669c69efdb0a946f161965dcf85bfb87b5b
kabul2022 <165707794+kabul2022@users.noreply.github.com>
2024-04-18 11:04:38 +0200

    added the basic search engine behavior

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+
+function hanndleSearch(event) {
+  event.preventDefault();
+  
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML= searchInput.value;
+}
+
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchsubmit);
+searchFormElement.addEventListener("submit",hanndleSearch);
+
+
