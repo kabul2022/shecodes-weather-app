846666653ea6f90e921d34f6e937e7e512436efb
kabul2022 <165707794+kabul2022@users.noreply.github.com>
2024-04-18 13:26:31 +0200

    added the search functionality  using the Api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+   let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML= Math.round(temperature);
+
+  
+}
+
+
+function searchCity(city) {
+  let apikey = "bb01a8cf18ea9o4532af5fe21atc5f3a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 
 function hanndleSearch(event) {
   event.preventDefault();
-  
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML= searchInput.value;
+  
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",hanndleSearch);
 
-
+searchCity("Kabul");
