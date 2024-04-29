52c1d01b9dae61597c64c0bea1893696eea6fec6
kabul2022 <165707794+kabul2022@users.noreply.github.com>
2024-04-29 13:09:14 +0200

    added the forecast for all days

--- src/index.js
+++ src/index.js
@@ -58,22 +58,32 @@ function getForecast(city) {
   let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function displayForecast(response) {
-  console.log(response.data);
-  
+
+
   let forecastHTML = "";
-  response.data.daily.forEach(function(day) {
+  response.data.daily.forEach(function (day,index) {
+    if(index < 5);
     forecastHTML += `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">
-        <img src ="${day.condition.icon_url}"</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        
+        <img src ="${day.condition.icon_url}" class="weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
           
         </div>
       </div>
@@ -83,7 +93,6 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHTML;
 }
-  
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",hanndleSearch);
